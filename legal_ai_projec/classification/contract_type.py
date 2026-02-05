f988393501466400d590e629d49e45e6cc18bf16
Sharfudeen2004 <mohamedsharfu2005@gmail.com>
2026-02-05 11:24:50 +0530

    Delete legal_ai_projec/classification directory

--- legal_ai_projec/classification/contract_type.py
+++
@@ -1,9 +0,0 @@
-def classify_contract(text):
-    t = text.lower()
-    if "employee" in t:
-        return "Employment Contract"
-    if "lease" in t:
-        return "Lease Agreement"
-    if "vendor" in t or "supplier" in t:
-        return "Vendor Contract"
-    return "General Service Contract"
